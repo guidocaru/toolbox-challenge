71e2d7916e285b32b06ce450c9c4c035474b60c0
gcarugati <carugatiguido@gmail.com>
2023-10-04 02:49:59 -0300

    config response status

--- api/controllers/files.js
+++ api/controllers/files.js
@@ -3,7 +3,7 @@ import { getFilesData } from "../services/client/index.js";
 export class FilesController {
   static async getFilesData(req, res) {
     const filesData = await getFilesData();
-
-    res.json(filesData);
+    if (filesData) return res.json(filesData);
+    res.status(500).json({ message: "an error occured" });
   }
 }
