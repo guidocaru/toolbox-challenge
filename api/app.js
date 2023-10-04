5f3e881fb0e9cdeade69d5a912ceab425db6fc7e
gcarugati <carugatiguido@gmail.com>
2023-10-04 02:42:30 -0300

    add cors config

--- api/app.js
+++ api/app.js
@@ -1,13 +1,15 @@
 import express, { json } from "express";
 import { filesRouter } from "./routes/files.js";
+import { corsMiddleware } from "./middlewares/cors.js";
 
 const app = express();
 app.use(json());
+app.use(corsMiddleware());
 
 app.use("/files", filesRouter);
 
 const PORT = process.env.PORT ?? 1234;
 
 app.listen(PORT, () => {
-  console.log(`app running on port ${PORT}`);
+  console.log(`server running on port ${PORT}`);
 });
