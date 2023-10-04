3fffe61dc8ff48a264568f4e95e62dadb6d68678
gcarugati <carugatiguido@gmail.com>
2023-10-04 03:37:07 -0300

    table component setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,14 @@
-function App() {
+import Table from "./Table.jsx";
+
+const App = () => {
   return (
-    <>
+    <main>
       <div>
         <h1>Toolbox Challenge</h1>
+        <Table></Table>
       </div>
-    </>
+    </main>
   );
-}
+};
 
 export default App;
---
+++ frontend/src/Table.jsx
@@ -0,0 +1,23 @@
+import Table from "react-bootstrap/Table";
+
+const FilesTable = () => {
+  return (
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>File Name</th>
+          <th>Text</th>
+          <th>Number</th>
+          <th>Hex</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr></tr>
+        <tr></tr>
+        <tr></tr>
+      </tbody>
+    </Table>
+  );
+};
+
+export default FilesTable;
